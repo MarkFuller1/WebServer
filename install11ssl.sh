eacde870acd54053e49d5734e79c78c1ecbe2efe
MarkFuller1 <Mark.Fuller1@baylorl.edu>
2019-08-15 02:59:36 +0100

    Remove dotnet

--- install11ssl.sh
+++
@@ -1,5 +0,0 @@
-tar xzvf openssl-1.1.0f.tar.gz
-cd openssl-1.1.0f
-./config -Wl,--enable-new-dtags,-rpath,'$(LIBRPATH)'
-make
-sudo make install
--- untaraspruntime.sh
+++
@@ -1,1 +0,0 @@
-tar zxf aspnetcore-runtime-2.2.1-linux-arm.tar.gz -C $HOME/dotnet
