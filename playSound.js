644df960db92a960116fb7c552dcd5b8a558b77b
MarkFuller1 <Mark.Fuller1@baylorl.edu>
2019-08-17 08:36:28 +0100

    more waves

--- playSound.js
+++ playSound.js
@@ -1,25 +1,37 @@
 //server.js
 const express = require('express'),
         server = express(),
-        Sound = require('node-aplay');
+        Sound = require('node-aplay'),
+        path = require('path')
 
 var portNum = 3000;
+
+server.use(express.static(path.join(__dirname, 'public')));
 
 //setting the port.
 server.set('port', process.env.PORT || portNum);
 
 //Adding routes
 server.get('/',(request,response)=>{
-        response.sendFile(__dirname + '/index.html');
+        response.sendFile(path.join(path.join(__dirname, 'views'), 'index.html'));
 });
 
-server.get('/playsound',(request,response)=>{
+server.get('/test' ,(request,response)=>{
         console.log('Success');
 
         //play sound
         var tone = new Sound('2000.wav').play();
 
         response.status(200);
+});
+
+server.get('/playsound', (request, response)=>{
+	console.log('Success');
+
+	//play sound
+	var tone = new Sound('48000.wav').play();
+
+	response.status(200);
 });
 
 //Binding to localhost://portNum
